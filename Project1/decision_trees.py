b09eb92c7b2c070590750e88deb5fb2130f9f33e
Natalie Arnold <narnold@nevada.unr.edu>
2019-09-17 09:28:59 -0700

    creating functions, no implementation yet. Successfully installed and imported numpy

---
+++ Project1/decision_trees.py
@@ -0,0 +1,12 @@
+import numpy
+
+def DT_traing_binary(X, Y, max_depth):
+
+    print("I am training binary")
+
+def DT_test_binary(X, Y, DT):
+    print("I am test binary")
+
+if __name__ == "__main__":
+
+
