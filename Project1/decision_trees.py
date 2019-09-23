b277510210998412140146d872865a053782b0a2
Natalie Arnold <narnold@nevada.unr.edu>
2019-09-23 10:46:36 -0700

    Fixed end conditions

--- Project1/decision_trees.py
+++ Project1/decision_trees.py
@@ -9,61 +9,116 @@ class Node:
         self.children = {'left': lchild, 'right': rchild}
 
     def printNode(self):
-        print(self.featureIndex)
-        print(self.children['left'])
-        print(self.children['right'])
+        if self.featureIndex != -1:
+            if self.children['left'] != 0:
+                print(self.children['left'].featureIndex)
+                self.children['left'].printNode()
+            if self.children['right'] != 0:
+                print(self.children['right'].featureIndex)
+                self.children['right'].printNode()
 
 
 class TrainBinary:
 
     def __init__(self):
-        self.featureData = 0
-        self.labelData = 0
+        self.featureData = numpy.array([[]])
+        self.labelData = numpy.array([[]])
         self.trainingTree = 0
 
+    def entropy(self, probability):
+        if probability <= 0:
+            return 0
+        else:
+            return -probability * numpy.log2(probability)
+
     def recursive_train_binary(self, samples, features, depth):
-        total_samples = len(samples)
-        left_samples = numpy.array([])
-        right_samples = numpy.array([])
-        feature_true_label_true = 0
-        feature_false_label_true = 0
 
-        print(self.featureData)
+        if depth != 0 and len(features) != 0:
+            total_samples = len(samples)
+            left_samples = numpy.array([])
+            right_samples = numpy.array([])
+            feature_data = {'information_gain': 0}
+            set_entropy = 0
 
-        for feature in features:
+            for feature in features:
+                feature_true_label_true = 0
+                feature_false_label_true = 0
+                # Looking at feature for each sample, collect data
+                for sample in samples:
 
-            # Looking at feature for each sample, collect data
-            for sample in samples:
-                if self.featureData[sample][feature]:
+                    if self.featureData[int(sample)][int(feature)]:
+                        # Track samples on right of split
+                        right_samples = numpy.append(right_samples, sample)
 
-                    # Track samples on right of split
-                    numpy.append(right_samples, sample)
-                    if self.labelData[sample][0]:
+                        if self.labelData[int(sample)][0]:
+                            # Count number of true labels on right of split
+                            feature_true_label_true += 1
+                    else:
 
-                        # Count number of true labels on right of split
-                        feature_true_label_true += 1
+                        # Track samples on left of split
+                        left_samples = numpy.append(left_samples, sample)
+                        if self.labelData[int(sample)][0]:
+                            # Count number of true labels on right of split
+                            feature_false_label_true += 1
+
+                # If either side has zero samples, information gain will be 0
+                if len(left_samples) != 0 and len(right_samples != 0):
+                    # Calculate entropy on the left side of the split on feature
+                    p_label_true = feature_false_label_true / len(left_samples)
+                    p_label_false = 1 - p_label_true
+
+                    label_left = 1 if p_label_true > .5 else 0
+
+                    entropy_left = self.entropy(p_label_true) + self.entropy(p_label_false)
+
+                    # Calculate entropy on the right side of the split on feature
+                    p_label_true = feature_true_label_true / len(right_samples)
+                    p_label_false = 1 - p_label_true
+
+                    label_right = 1 if p_label_true > .5 else 0
+
+                    entropy_right = self.entropy(p_label_true) + self.entropy(p_label_false)
+
+                    # Calculate entropy of whole set
+                    p_label_true = (feature_false_label_true + feature_true_label_true) / total_samples
+                    p_label_false = 1 - p_label_true
+                    set_entropy = self.entropy(p_label_true) + self.entropy(p_label_false)
+
+                    # Calculate information gain of feature and save all necessary information of feature (IG,
+                    # samples left, samples right, label left, label right)
+
+                    information_gain = set_entropy - (len(left_samples) / total_samples) * entropy_left - \
+                                       (len(right_samples) / total_samples) * entropy_right
+
+                    # If this is the best feature to split on so far update feature info
+                    if information_gain > feature_data['information_gain']:
+                        feature_data['information_gain'] = information_gain
+                        feature_data['feature_index'] = feature
+                        feature_data['left_samples'] = left_samples
+                        feature_data['right_samples'] = right_samples
+                        feature_data['label_left'] = label_left
+                        feature_data['label_right'] = label_right
+
+            if 'feature_index' in feature_data:
+                # Remove chosen feature from list of possible features
+                features = features[features != feature_data['feature_index']]
+
+                # Create new tree node and call function on left and right samples if we haven't hit max IG
+                if feature_data['information_gain'] != set_entropy:
+                    # index, llabel, rlabel, lchild, rchild
+                    return Node(feature_data['feature_index'], feature_data['label_left'], feature_data['label_right'],
+                                self.recursive_train_binary(feature_data['left_samples'], features, depth - 1),
+                                self.recursive_train_binary(feature_data['right_samples'], features, depth - 1))
                 else:
+                    return Node(feature_data['feature_index'], feature_data['label_left'], feature_data['label_right'],
+                                0, 0)
+            else:
+                # No feature to split on (None of the features improve entropy ~ IG = 0)
+                return Node(-1, 0, 0, 0, 0)
 
-                    # Track samples on left of split
-                    numpy.append(left_samples, sample)
-                    if self.labelData[sample][0]:
-
-                        # Count number of true labels on right of split
-                        feature_false_label_true += 1
-
-            # Calculate entropy on the left side of the split on feature
-
-            # Calculate entropy on the right side of the split on feature
-
-            # Calculate entropy of whole set
-
-            # Calculate information gain of feature and save all necessary information of feature
-
-        # Determine best feature to split on
-
-        # Create new tree node and call function on left and right samples (if IG != H())
-
-        return Node(0, 0, 0, 0, 0)
+        else:
+            # No feature to split on (max depth reached or no more features)
+            return Node(-1, 0, 0, 0, 0)
 
 
 def DT_train_binary(X, Y, max_depth):
@@ -81,7 +136,8 @@ def DT_test_binary(X, Y, DT):
 
 
 if __name__ == "__main__":
-
-    x = numpy.array([[0, 1], [0, 0], [1, 0], [0, 0],  [1, 1]])
-    y = numpy.array([[1], [0], [0], [0], [1]])
-    DT_train_binary(x, y, -1).printNode()
+    x = numpy.array([[0, 1, 0, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 0, 1, 1], [1, 1, 0, 1], [1, 1, 0, 0], [1, 0, 0, 1], [0, 1, 0, 1], [0, 1, 0, 0]])
+    y = numpy.array([[0], [1], [0], [0], [1], [0], [1], [1], [1]])
+    tree = DT_train_binary(x, y, -1)
+    print(tree.featureIndex)
+    tree.printNode()
